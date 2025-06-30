7f9d13b05d542675693ded3278c025c863359e1b
dafab8 <dafinaab1@gmail.com>
2025-06-30 14:27:16 +0200

    cards done

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Photos } from '../photos';
+import { IPhoto } from '../iphoto';
 
 @Component({
   selector: 'app-portfolio',
   imports: [],
   templateUrl: './portfolio.html',
-  styleUrl: './portfolio.css'
+  styleUrl: './portfolio.css',
 })
 export class Portfolio {
-
+  photos: IPhoto[] = Photos;
 }
