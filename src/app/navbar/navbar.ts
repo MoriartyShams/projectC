649f465c73ebb80afa416781439b639c64f8a955
Moriarty <mariasworld9@gmail.com>
2025-06-30 22:05:18 +0200

    add filtering on navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,22 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { Filtering } from '../filtering';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   categories: string[] = [];
+  isMenuOpen: boolean = false;
  
 
 
-  constructor(private filtering: Filtering) {
+  constructor(private filtering: Filtering, private router: Router) {
     this.categories = this.filtering.addCategoriesToNavbar();
   }
+
+
+   toggleMenu(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isMenuOpen = !this.isMenuOpen;
+   }
+ 
   addCategory(category: string) {
-    this.filtering.setCategory(category)
+    this.filtering.setCategory(category);
+    this.isMenuOpen = false;
+  }
+
+ 
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown')) {
+      this.isMenuOpen = false;
+    }
   }
 }
