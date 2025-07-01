6914243ccb389769d7862a27416f7aa9c2cc40a7
Azra Avdovic <azra.avdovic01@gmail.com>
2025-07-01 13:16:29 +0200

    Base Testimonary

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Ireview } from '../Ireview';
+import { Review } from '../review';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
+  review: Ireview[] = Review;
 
 }
