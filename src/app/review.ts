98b3178df044c8c4ba64f03adfd5df82b8ef61c7
Azra Avdovic <azra.avdovic01@gmail.com>
2025-07-01 12:07:01 +0200

    Ceating Interface for the testimonary

---
+++ src/app/review.ts
@@ -0,0 +1,34 @@
+import { Ireview } from './Ireview';
+
+export const Review: Ireview[] = [
+  {
+    id: 1,
+    title: 'Fantastic',
+    name: 'Cait Smith',
+    description: 'Beautiful pictures and the photographes are very kind',
+    rating: 4,
+  },
+  {
+    id: 2,
+    title: 'Best Pictures',
+    name: 'Samantha Köller',
+    description:
+      'I took the Potrait´s offer for my family and they all look amazing. Will recommend it everywhere',
+    rating: 5,
+  },
+  {
+    id: 3,
+    title: 'Quick and Nice',
+    name: 'Bruce Wayne',
+    description: 'I didn´t have to wait more than 4 days to get my pictures',
+    rating: 4,
+  },
+  {
+    id: 4,
+    title: 'Fluffy likes them',
+    name: 'Timothy Shrein',
+    description:
+      'My cat, Fluffy, likes the photographs, she usually is never this nice. The photograph was really patient and the pictures were a hit',
+    rating: 5,
+  },
+];
